f89c6f645a9806fdbd556f57598b44fbd1582837
pipe <felipe.parra@blackmind.cl>
2023-05-13 02:30:35 -0400

    apuntando al back del hosting fly.io

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //HOST: 'https://ecopuntos.fly.dev/rest/v1',
-  HOST: 'http://localhost:8080/rest/v1',
+  HOST: 'https://ecopuntos.fly.dev/rest/v1',
+  // HOST: 'http://localhost:8080/rest/v1',
 
 };
